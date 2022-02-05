4378b416dc2cb28e6b6d8edc57448eaaa07f0023
Asis Melgarejo <asis.melgarejo@2myr.com>
2022-02-04 19:30:28 -0500

    fix6

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -2,22 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { homeRoutes } from "./nav-routes";
 import { PrivateRoute } from "./PrivateRoute";
+import { Home } from "../views/Home";
 
 const AllRoutes = () => {
-  const moduleHome = homeRoutes.map(({ path, name, Component }, key) => (
-    <PrivateRoute
-      key={`${key}-${name}`}
-      path={path}
-      exact
-      component={({ location }: any) => {
-        return <Component />;
-      }}
-    />
-  ));
+  // const moduleHome = homeRoutes.map(({ path, name, Component }, key) => (
+  //   <PrivateRoute
+  //     key={`${key}-${name}`}
+  //     path={path}
+  //     exact
+  //     component={({ location }: any) => {
+  //       return <Component />;
+  //     }}
+  //   />
+  // ));
   return (
-    <Router>
-      <Switch>{moduleHome}</Switch>
-    </Router>
+    <div>
+      {/* <Switch>{moduleHome}</Switch> */}
+      <Home/>
+    </div>
   );
 };
 
